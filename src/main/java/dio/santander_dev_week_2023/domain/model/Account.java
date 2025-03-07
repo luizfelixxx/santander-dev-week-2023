6b6bafb1e98ec0afa5ab14c0df2d58774cf0b8e7
LuizFelixxx <luizfelix.dev@gmail.com>
2025-03-07 12:11:22 -0300

    🐛 fix: swap precision and scale values in Card and Account classes

--- src/main/java/dio/santander_dev_week_2023/domain/model/Account.java
+++ src/main/java/dio/santander_dev_week_2023/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
     @Column(nullable = false)
     private String agency;
 
-    @Column(precision = 2, scale = 13, nullable = false)
+    @Column(precision = 13, scale = 2, nullable = false)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit",precision = 2, scale = 13, nullable = false)
+    @Column(name = "additional_limit",precision = 13, scale = 2, nullable = false)
     private BigDecimal limit;
 
     public Long getId() {
